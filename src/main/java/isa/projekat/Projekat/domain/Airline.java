389621d1b7b9f3687b7979e33d2655ac3f8ebaa5
LjRokvic <theprotonwave@gmail.com>
2018-12-07 18:25:36 +0100

    Initial Airline model.

---
+++ src/main/java/isa/projekat/Projekat/domain/Airline.java
@@ -0,0 +1,52 @@
+package isa.projekat.Projekat.domain;
+
+import java.util.ArrayList;
+
+public class Airline {
+
+    private String name;
+    private String address;
+    private String description;
+    private ArrayList<Double> grades;
+    private ArrayList<String> destinations;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<Double> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Double> grades) {
+        this.grades = grades;
+    }
+
+    public ArrayList<String> getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(ArrayList<String> destinations) {
+        this.destinations = destinations;
+    }
+}
