903249e9a9df0991d167af1bb554f84813aad3e4
DiligentHero <theprotonwave@gmail.com>
2018-12-07 17:28:50 +0100

    Initial project

---
+++ src/main/java/isa/projekat/Projekat/ProjekatApplication.java
@@ -0,0 +1,12 @@
+package isa.projekat.Projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatApplication.class, args);
+	}
+}
---
+++ src/test/java/isa/projekat/Projekat/ProjekatApplicationTests.java
@@ -0,0 +1,16 @@
+package isa.projekat.Projekat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjekatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
