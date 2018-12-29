400a4d8fd1cea1006646226b200274bdb1e59680
LjRokvic <ljubomirrokvic@gmail.com>
2018-12-29 16:21:52 +0100

    Fix

--- src/main/java/isa/projekat/Projekat/controller/airline/AirlineController.java
+++ src/main/java/isa/projekat/Projekat/controller/airline/AirlineController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class AirlineController {
 
     @Autowired
