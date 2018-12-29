35376d385df41e17d0b130014238db1a901410f3
Gorluxor <cvejicaca96@gmail.com>
2018-12-29 16:33:37 +0100

    project structure

--- src/main/java/isa/projekat/Projekat/repository/CarRepository.java
+++ src/main/java/isa/projekat/Projekat/repository/CarRepository.java
@@ -1,6 +1,6 @@
 package isa.projekat.Projekat.repository;
 
-import isa.projekat.Projekat.model.RentACar.Cars;
+import isa.projekat.Projekat.model.rent_a_car.Cars;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
