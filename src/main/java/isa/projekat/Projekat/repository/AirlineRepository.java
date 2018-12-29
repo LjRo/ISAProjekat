ebdfa78e062fc11921ea54390fcc1f09d01f7b17
LjRokvic <ljubomirrokvic@gmail.com>
2018-12-29 15:49:58 +0100

    Added component annotation.

--- src/main/java/isa/projekat/Projekat/repository/AirlineRepository.java
+++ src/main/java/isa/projekat/Projekat/repository/AirlineRepository.java
@@ -2,7 +2,9 @@ package isa.projekat.Projekat.repository;
 
 import isa.projekat.Projekat.model.Airline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface AirlineRepository extends JpaRepository<Airline, Long> {
 
 }
--- src/main/java/isa/projekat/Projekat/repository/UserRepository.java
+++ src/main/java/isa/projekat/Projekat/repository/UserRepository.java
@@ -2,7 +2,9 @@ package isa.projekat.Projekat.repository;
 
 import isa.projekat.Projekat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
