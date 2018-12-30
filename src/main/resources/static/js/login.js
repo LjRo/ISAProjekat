2117ae8585b5ad2d0963d3c89a05ddd354577a47
Gorluxor <cvejicaca@gmail.com>
2018-12-30 20:21:39 +0100

    set access token local storage

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -8,8 +8,14 @@ $(document).ready(function () {
         $.post({
             url: window.location.href.match(/^.*\//) + "auth/login",
             data: JSON.stringify({username:username, password: password}),
+            headers: {"accessToken": localStorage.getItem('accessToken'), "expiresIn": localStorage.getItem('expiresIn')},
             contentType: 'application/json',
-            success: function() {
+            success: function(data) {
+
+                localStorage.setItem('accessToken', data['accessToken']);
+                localStorage.setItem('expiresIn',data['expiresIn']);
+
+
                 window.location.replace("http://localhost:8080/index.html");
             },
             error: function() {
