1139fda2d762db8f53727511f7dd1a7f9b0bd2be
Gorluxor <cvejicaca@gmail.com>
2018-12-30 19:40:00 +0100

    added users with password 123

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -36,18 +36,13 @@ values ("Spora","Mercedes","C","44WE32",4,4,4,100,false,1);
 
 
 /* users.sql */
+/* password = 123*/
 
 INSERT into Users (type, username, email, first_name, last_name, password, address, city, enabled, phone_number)
-values (0,"normal","normal@gmail.com","pera","peric","$10$M7ooixCj3tCNjIvHecXVAOnS6Myx0gFMmi0nSf6s2lwp/8ylNsdy.","adr1","city1",b'1',"123-456-1234");
+values (0,"normal","normal@gmail.com","pera","peric","$2a$10$UwDhAk3jFirZEec1o3pEMeVnUPws.nJG2N3AxjmmyTYVMjOsmB3ku","adr1","city1",b'1',"123-456-1234");
 
 INSERT into Users (type, username, email, first_name, last_name, password, address, city, enabled, phone_number)
-values (1,"admin","admin@gmail.com","zika","zikic","$10$M7ooixCj3tCNjIvHecXVAOnS6Myx0gFMmi0nSf6s2lwp/8ylNsdy.","adr1","city1",b'1',"123-456-1234");
+values (0,"normalA","normala@gmail.com","normal","normalic","$2a$10$3eTRX.1YlWwnP/j7oeMRpeFP.DA9UhTuUoxO.jMqLXHpIITohxPjW","adr0","city0",b'1',"123-456-1234");
 
 INSERT into Users (type, username, email, first_name, last_name, password, address, city, enabled, phone_number)
-values (2,"airline","airline@gmail.com","airline","airlineovic","$10$M7ooixCj3tCNjIvHecXVAOnS6Myx0gFMmi0nSf6s2lwp/8ylNsdy.","adr1","city1",b'1',"123-456-1234");
-
-INSERT into Users (type, username, email, first_name, last_name, password, address, city, enabled, phone_number)
-values (3,"hotel","hotel@gmail.com","hotel","hotelic","$10$M7ooixCj3tCNjIvHecXVAOnS6Myx0gFMmi0nSf6s2lwp/8ylNsdy.","adr1","city1",b'1',"123-456-1234");
-
-INSERT into Users (type, username, email, first_name, last_name, password, address, city, enabled, phone_number)
-values (4,"rent","rent@gmail.com","rent","retnic","$10$M7ooixCj3tCNjIvHecXVAOnS6Myx0gFMmi0nSf6s2lwp/8ylNsdy.","adr1","city1",b'1',"123-456-1234");
+values (1,"admin","admin@gmail.com","zika","zikic","$2a$10$6GTuwgfxgVEqQJt1lWsPu.4yiN8ktBAP7JABIcnbehHE7bybpIDKG","adr2","city2",b'1',"123-456-1234");
