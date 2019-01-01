ab0bb8440089f665566ea9f97026f77d4a7fd2f4
Astral <andrijacvejic@gmail.com>
2019-01-01 18:53:28 +0100

    Adress change in model fix for hotel.

--- src/main/resources/static/js/hotels.js
+++ src/main/resources/static/js/hotels.js
@@ -24,7 +24,7 @@ function addArticle(hotel) {
         '<a href="/hotelprofile.html?id=' + hotel.id + '">' +
         '<h3 class="name">' +hotel.name +'</h3>' +
         '</a>' +
-        '<p class="description">Address: <span style = "color:black">'+ hotel.address + '</span></p>' +
+        '<p class="description">Address: <span style = "color:black">'+ hotel.address.addressName  + ',' + hotel.address.city + '</span></p>' +
         '<p class="description">'+ hotel.description +'</p>' +
         '<a class="edit-hotel admin" href="edithotel.html?id=' + hotel.id +'&name='+ hotel.name +'&description='+ hotel.description + '"><img src="/../assets/img/edit.png" style="height:16px;width16px;"></a> ' +
         '<a id="' + hotel.id + '" class="delete-hotel admin" href="hotels.html"><img src="assets/img/delete.png" style="height:16px;width16px;"></a> '+
