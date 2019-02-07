50bf680c0f648eb5faaa6913a666b5926bbef539
Astral <andrijacvejic@gmail.com>
2019-02-07 20:56:19 +0100

    Changes to model.

--- src/main/java/isa/projekat/Projekat/repository/OrderRepository.java
+++ src/main/java/isa/projekat/Projekat/repository/OrderRepository.java
@@ -14,7 +14,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByPlacedOrderId(Long id);
 
 
-    @Query(value = "SELECT o.* FROM orders o LEFT OUTER JOIN order_reservations os  ON o.id = os.id LEFT OUTER JOIN reservations r ON os.reservation_id = r.id" +
+    @Query(value = "SELECT o.* FROM orders o LEFT OUTER JOIN orders_reservations os  ON o.id = os.order_id LEFT OUTER JOIN reservations r ON os.reservations_id = r.id" +
             " LEFT OUTER JOIN flights f ON r.flight_id = f.id WHERE" +
             " f.start_time >=?2 AND r.user_id = ?1", nativeQuery = true)
     List<Order> findAllFutureOrdersByUserIdAndDate(Long userId, String todayDate);
