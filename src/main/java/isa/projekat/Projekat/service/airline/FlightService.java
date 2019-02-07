ce804c592b202dfeb675abf6cb3049ecd76b5e23
Gorluxor <cvejicaca@gmail.com>
2019-02-07 23:28:49 +0100

    added back the method that has been removed findOrderById Fixed fast reservations now linked in order

--- src/main/java/isa/projekat/Projekat/service/airline/FlightService.java
+++ src/main/java/isa/projekat/Projekat/service/airline/FlightService.java
@@ -13,6 +13,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FlightService {
@@ -41,6 +42,14 @@ public class FlightService {
         }
     }
 
+    @Transactional(readOnly = true)
+    public Order findOrderById(Long id){
+        Optional<Order> ord = orderRepository.findById(id);
+        if (!ord.isPresent())
+            return null;
+        else
+            return ord.get();
+    }
 
     @Transactional(readOnly = true)
     public SeatData findSeatDataById(Long id) {
